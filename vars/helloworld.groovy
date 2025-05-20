486f40861d735e28a8cd0d6f9524260b1219647e
pvikas241 <62456590+pvikas241@users.noreply.github.com>
2025-05-20 16:35:04 +0530

    Update helloworld.groovy

--- vars/helloworld.groovy
+++ vars/helloworld.groovy
@@ -1,7 +1,6 @@
 def call() {
         script {
-            echo "Before error..."
-            error("Intentional failure for testing")
-            currentBuild.result = 'FAILURE'  // Explicitly mark pipeline as failed
+            echo "helloworld is succesfull"
+
         }
 }
