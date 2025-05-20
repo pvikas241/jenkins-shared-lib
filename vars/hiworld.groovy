8d55dc068d3ed5da86ab7970eff50265256bc763
pvikas241 <62456590+pvikas241@users.noreply.github.com>
2025-05-20 16:25:45 +0530

    Update hiworld.groovy

--- vars/hiworld.groovy
+++ vars/hiworld.groovy
@@ -1,7 +1,5 @@
 def call() {
-        script {
-            echo "Before error..."
-            error("Intentional failure for testing")
-            currentBuild.result = 'FAILURE'  // Explicitly mark pipeline as failed
+    stage('Hi World') {
+    echo "Hi World from Shared Library!"
         }
     }
